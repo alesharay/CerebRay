bef4807ce01dd4d491ce52f5d922602dca7a4e2a
Alesha Ray <alesha.ray@g.austincc.edu>
2026-04-16 18:49:14 -0500

    update route change

--- frontend/src/faro.ts
+++ frontend/src/faro.ts
@@ -1,5 +1,17 @@
-import { initializeFaro, getWebInstrumentations } from '@grafana/faro-react';  
+import {  
+  initializeFaro,  
+  getWebInstrumentations,  
+  ReactIntegration,  
+  createReactRouterV7Options,  
+} from '@grafana/faro-react';  
 import { TracingInstrumentation } from '@grafana/faro-web-tracing';  
+import {  
+  createRoutesFromChildren,  
+  matchRoutes,  
+  Routes,  
+  useLocation,  
+  useNavigationType,  
+} from 'react-router-dom';  
 
 initializeFaro({  
   url: 'https://faro-collector-prod-us-central-0.grafana.net/collect/0aedb11d9e4e5952c403b45e9dcc2d5c',  
@@ -8,7 +20,19 @@ initializeFaro({
     version: '0.0.0',  
     environment: import.meta.env.MODE,  
   },  
-  instrumentations: [...getWebInstrumentations(), new TracingInstrumentation()],  
+  instrumentations: [  
+    ...getWebInstrumentations(),  
+    new TracingInstrumentation(),  
+    new ReactIntegration({  
+      router: createReactRouterV7Options({  
+        createRoutesFromChildren,  
+        matchRoutes,  
+        Routes,  
+        useLocation,  
+        useNavigationType,  
+      }),  
+    }),  
+  ],  
   ignoreErrors: [  
     /^ResizeObserver loop limit exceeded$/,  
     /^ResizeObserver loop completed with undelivered notifications$/,  
